e82cfe7c0e52db4be72487b639bc018d22ee1fd6
asneakytoast <joelnotlithgow@gmail.com>
2023-09-10 22:37:35 -0700

    Upgraded to astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thisisahousegallery.com",
+  site: "https://thisisahousegallery.com/",
   integrations: [vue(), svelte()]
 });
