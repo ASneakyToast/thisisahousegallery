28b954f1ba04ad8f0c18edadd20d9e1b6fcd2906
asneakytoast <joelnotlithgow@gmail.com>
2023-12-27 09:54:58 -0800

    Updated packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-import { astroImageTools } from "astro-imagetools";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://thisisahousegallery.com/",
-  integrations: [vue(), svelte(), astroImageTools]
+  integrations: [vue(), svelte()]
 });
